b37f03d601ebc4924583f4fcaf1a4b879cbc4085
glebstav <gstav2000@gmail.com>
2020-05-08 01:38:11 +0700

    error handling and extension

--- 5lab/5lab.c
+++ 5lab/5lab.c
@@ -1,12 +1,22 @@
 #include <sys/types.h>
+#include <sys/time.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 const int MAX_COUNT_OF_LINES = 10000;
 const int MAX_SYMBOLS_IN_LINE = 1000;
 
+void freeAll(unsigned int cnt, ...){
+    void* pointer = &cnt + 1;
+    while(cnt--){
+	free(pointer);
+	pointer++;	
+    }
+
+}
 
 
 int SearchNextLine(char *line,int *pos,int symbolsInLine){
@@ -45,6 +55,7 @@ void LineDivider(char *currLine,int *numOfLine, int *tableOfOffset, int symbolsI
 
 
 int main(int argv,char **argc) {
+    unsigned int needToFree = 0;  
     int *tableOfOffset;
     int numOfLine = 0;
     int symbolsRead;
@@ -53,16 +64,59 @@ int main(int argv,char **argc) {
     char *nameOfFile;
     int fileDescriptor;
     int line=1;
+
+	//memory allocation
+
     tableOfOffset = calloc(MAX_COUNT_OF_LINES,sizeof(int));
+    if(tableOfOffset==0){
+	perror("malloc failed");
+	return 0;
+    }
+    needToFree++;
     currLine = malloc(sizeof(char)*MAX_SYMBOLS_IN_LINE);
+    if(currLine == 0){
+	perror("malloc failed");
+	freeAll(needToFree,tableOfOffset);
+	return 0;
+    }
+    needToFree++;
     nameOfFile = malloc(sizeof(char)*MAX_SYMBOLS_IN_LINE);
+    if(nameOfFile==0){
+	perror("malloc failed");
+	freeAll(needToFree,tableOfOffset,currLine);
+	return 0;
+    }
+    needToFree++;
+
+	//file opening
+
     fileDescriptor=open(argc[1],O_RDONLY);
     while(fileDescriptor==-1){
 	printf("wrong name of file! try again:\n");
 	scanf("%s",nameOfFile);
 	fileDescriptor=open(nameOfFile,O_RDONLY);
     }
-    symbolsRead = read(fileDescriptor,currLine,MAX_SYMBOLS_IN_LINE);  
+
+	//file reading
+
+    symbolsRead = read(fileDescriptor,currLine,MAX_SYMBOLS_IN_LINE);
+    while(symbolsRead<0){
+	if(errno == EINTR){
+	    errno = 0;
+	    symbolsRead = read(fileDescriptor,currLine,MAX_SYMBOLS_IN_LINE);
+	    continue;
+	}
+	perror("can't read from file");
+	freeAll(needToFree,tableOfOffset,currLine,nameOfFile);
+	if(close(fileDescriptor)==-1){
+	    perror("can't close file");
+	    return -1;
+	}
+	return 0;
+    }	  
+
+	//file proccessing
+
     while(symbolsRead==MAX_SYMBOLS_IN_LINE){
 	
 	LineDivider(currLine,&numOfLine,tableOfOffset,symbolsRead);
@@ -82,7 +136,17 @@ int main(int argv,char **argc) {
 	    for(int i=0;i<line-1;i++){
 	        sumOfOffset+=tableOfOffset[i];
 	    }
-	    lseek(fileDescriptor,sumOfOffset,SEEK_SET);
+
+	    if(lseek(fileDescriptor,sumOfOffset,SEEK_SET)==-1){
+		perror("lseek failed");
+		freeAll(needToFree,currLine,tableOfOffset,nameOfFile);
+		if(close(fileDescriptor)==-1){
+	            perror("can't close file");
+        	}
+	        return -1;
+	    }
+	
+
 	    read(fileDescriptor,currLine,tableOfOffset[line-1]);
 	    for(int i=0;i<tableOfOffset[line-1];i++){
 	        printf("%c",currLine[i]);
@@ -95,10 +159,12 @@ int main(int argv,char **argc) {
 	    }
 	}
     }
-    close(fileDescriptor);
-    free(currLine);
-    free(tableOfOffset);
-    free(nameOfFile);
+    freeAll(needToFree,currLine,tableOfOffset,nameOfFile);
+    if(close(fileDescriptor)==-1){
+            perror("can't close file");
+            return -1;
+    }
+
     return 0;
 }
 
